66d58b3b3040f303390b583225b0a614e9898cbf
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2021-03-17 21:25:57 -0400

    some html and css lol i dont know what im doing

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,16 @@
 console.log("Connected");
+
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+
+const quizQuestions = [
+  {
+    question: "Which character appears in the movie 'Cars'",
+    answers: {
+      a: "Barney",
+      b: "Tow Mater",
+      c: "Baymax",
+    },
+  },
+];
