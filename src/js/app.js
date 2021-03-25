51f301f64668d74fd97762367519099cccf56caa
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2021-03-25 19:02:29 -0400

    starting off fresh

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,6 @@
 console.log("Connected");
 
-const quizContainer = document.getElementById("quiz");
-const resultsContainer = document.getElementById("results");
-const submitButton = document.getElementById("submit");
-
-const quizQuestions = [
-  {
-    question: "Which character appears in the movie 'Cars'",
-    answers: {
-      a: "Barney",
-      b: "Tow Mater",
-      c: "Baymax",
-    },
-  },
-];
+const quizQuestions = {
+  question: "Which character appears in the movie 'Cars'",
+  answers: {},
+};
