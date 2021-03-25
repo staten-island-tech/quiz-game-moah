9febbb744a981826671d742695c96296569d9c7c
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2021-03-25 19:34:41 -0400

    part1

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,51 @@
 console.log("Connected");
 
-const quizQuestions = {
-  question: "Which character appears in the movie 'Cars'",
-  answers: {},
-};
+[
+  {
+    questionContent: "Are you more independent or dependent?",
+    choices: [
+      {
+        choice: "Independent",
+        value: 1,
+      },
+      {
+        choice: "Dependent",
+        value: 0,
+      },
+    ],
+  },
+  {
+    questionContent: "What is your favorite food?",
+    choices: [
+      {
+        choice: "Gravy",
+        value: 1,
+      },
+      {
+        choice: "Steak",
+        value: 3,
+      },
+      {
+        choice: "Rice",
+        value: 5,
+      },
+    ],
+  },
+  {
+    questionContent: "What is your favorite color out of these three?",
+    choices: [
+      {
+        choice: "Blue",
+        value: 1,
+      },
+      {
+        choice: "Steak",
+        value: 3,
+      },
+      {
+        choice: "Rice",
+        value: 5,
+      },
+    ],
+  },
+];
